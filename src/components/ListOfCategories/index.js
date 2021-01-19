05cfd2bf3cd6ff754cb92765b023bebfed721d32
Eduardo Diaz <lalo.diaz1896@gmail.com>
2021-01-19 17:06:40 -0600

    fixed categories with classname

--- src/components/ListOfCategories/index.js
+++ src/components/ListOfCategories/index.js
@@ -8,17 +8,42 @@ export function ListOfCategories() {
   const [categories, setCategories] = useState([])
 
   useEffect(() => {
-    fetch("https://petgram-api-wheat.vercel.app/categories")
-      .then(response => response.json())
-      .then(data => setCategories(data))
+    const fetchCategories = async () => {
+      const response = await fetch('https://petgram-api-wheat.vercel.app/categories')
+      const data = await response.json()
+      setCategories(data)
+    }
+    fetchCategories()
   }, [])
-  return (
-    <List>
+
+  const renderList = (fixed) => (
+    <List className={fixed ? "fixed" : ""}>
       {
         categories.map(category => (
           <li key={category.id}><Category cover={category.cover} emoji={category.emoji} path={category.path} /></li>
         ))
       }
     </List>
+  )
+
+  const [isFixed, setIsFixed] = useState(false)
+
+  useEffect(() => {
+    const handleScroll = (e) => {
+      const newShowFixed = window.scrollY > 200
+      isFixed !== newShowFixed && setIsFixed(newShowFixed)
+    }
+
+    document.addEventListener('scroll', handleScroll)
+
+    return () => document.removeEventListener('scroll', handleScroll)
+
+  }, [isFixed])
+
+  return (
+    <>
+      {renderList()}
+      {isFixed && renderList(true)}
+    </>
   )
 }
--- src/components/ListOfCategories/styles.js
+++ src/components/ListOfCategories/styles.js
@@ -1,4 +1,5 @@
 import styled from "styled-components"
+import { fadeIn } from "../../styles/animations"
 
 export const List = styled.ul`
   width: 100%;
@@ -7,6 +8,21 @@ export const List = styled.ul`
   /* justify-content: space-between; */
   /* align-items: center; */
   padding: .5em;
+
+  &.fixed {
+    background: #fff;
+    border-radius: 1em;
+    box-shadow: 0px 10px 14px rgba(0, 0, 0, 0.3);
+    position: fixed;
+    z-index: 1;
+    transform: scale(.65);
+    top: -10px;
+    left: 0;
+    right: 0;
+    margin: 0 auto;
+    max-width: 400px;
+    ${fadeIn({ time: '.75s' })};
+  }
 
   &::-webkit-scrollbar {
     display: none;
