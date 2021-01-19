c4a49c957d878f41351ddef92910d2d9ac770059
Eduardo Diaz <lalo.diaz1896@gmail.com>
2021-01-19 13:37:45 -0600

    useState, useEffect in Categories

--- src/components/ListOfCategories/index.js
+++ src/components/ListOfCategories/index.js
@@ -1,10 +1,17 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { Category } from "../Category"
 import { List } from "./styles"
 
-import { categories } from "../../../api/db.json"
+import { categories as MockCategories } from "../../../api/db.json"
 
 export function ListOfCategories() {
+  const [categories, setCategories] = useState([])
+
+  useEffect(() => {
+    fetch("https://petgram-api-wheat.vercel.app/categories")
+      .then(response => response.json())
+      .then(data => setCategories(data))
+  }, [])
   return (
     <List>
       {
