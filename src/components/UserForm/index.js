748c8029bf5bd926c6903b26f38dc90a69afc749
Eduardo Diaz <lalo.diaz1896@gmail.com>
2021-02-02 22:18:24 -0600

    Form de inicio de sesión

--- src/components/UserForm/index.js
+++ src/components/UserForm/index.js
@@ -1,10 +1,25 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 export const UserForm = ({ handleSubmit }) => {
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+    
     return (
         <form onSubmit={handleSubmit}>
-            <input placeholder="email"  onChange={e => e.target.value} />
-            <input placeholder="password" type="password" value="password" onChange={e => e.target.value} />
+            <input
+                placeholder="email"
+                
+                value={email}
+                onChange={e => setEmail(e.target.value)}
+            />
+            <input
+                placeholder="password"
+                type='password'
+                
+                value={password}
+                onChange={e => setPassword(e.target.value)}
+            />
+            
             <button type="submit">Inicia sesión</button>
         </form>
     )
